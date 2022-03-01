8bfdd845243c0320b3c5c4b068af58c5ff6faf9a
Christina Barcey <barceychristina@gmail.com>
2022-03-01 13:41:08 +0530

    template driven form implementation

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
+  floatingInput: string = '';
+  floatingTextarea: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  contactUsSubmit(formValue: NgForm){
+    console.log(formValue.value);
+    //validations
+    //Data Processing
+    //Then call API to save the data
   }
 
 }
